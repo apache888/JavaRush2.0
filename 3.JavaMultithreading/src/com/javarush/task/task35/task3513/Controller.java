49910241868c08aefbdc056f976e486b4338df71
apache888 <roman.haydarzhy@gmail.com>
2017-03-19 12:42:17 +0200

    19.03.17 end of signup

--- 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Controller.java
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Controller.java
@@ -1,13 +1,47 @@
 package com.javarush.task.task35.task3513;
 
 import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 /**
  * Create by Roman Hayda on 12.03.2017.
  */
 public class Controller extends KeyAdapter {
+    private static final int WINNING_TILE = 2048;
     private Model model;
     private View view;
+
+    public Controller(Model model) {
+        this.model = model;
+        this.view = new View(this);
+    }
+
+    public void resetGame() {
+        model.score =0;
+        view.isGameWon = false;
+        view.isGameLost = false;
+        model.resetGameTiles();
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_ESCAPE) resetGame();
+        if (!model.canMove()) {view.isGameLost = true;}
+        if (!view.isGameWon & !view.isGameLost) {
+            if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+                model.left();
+            } else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+                model.right();
+            } else if (e.getKeyCode() == KeyEvent.VK_UP) {
+                model.up();
+            } else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+                model.down();
+            }
+        }
+        if (model.maxTile == WINNING_TILE) view.isGameWon = true;
+
+        view.repaint();
+    }
 
     public Tile[][] getGameTiles() {
         return model.getGameTiles();
@@ -15,5 +49,9 @@ public class Controller extends KeyAdapter {
 
     public int getScore() {
         return model.score;
+    }
+
+    public View getView() {
+        return view;
     }
 }
--- 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Main.java
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Main.java
@@ -1,11 +1,26 @@
 package com.javarush.task.task35.task3513;
 
+import javax.swing.*;
+
 /**
  * Create by Roman Hayda on 12.03.2017.
  */
 public class Main {
     public static void main(String[] args) {
+        Model model = new Model();
+        Controller controller = new Controller(model);
+        JFrame game = new JFrame();
 
+        game.setTitle("2048");
+        game.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        game.setSize(450, 500);
+        game.setResizable(false);
+
+        game.add(controller.getView());
+
+
+        game.setLocationRelativeTo(null);
+        game.setVisible(true);
     }
 
 }
